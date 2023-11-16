8d3ebe2867d0c94e50012a1246c9df0b2e3457de
Liviu <petrealiviu1996@gmail.com>
2023-11-16 12:12:11 +0000

    Add sample scripts

---
+++ sample/saple_build_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Удаляем существующий Docker-образ
+docker image rm helloworld
+
+# Собираем новый Docker-образ
+docker build -t helloworld .
+
+# Запускаем контейнер
+docker run -d --name Hello_World helloworld

dda45e931cd9c7cf8801b713b3b0d892b6dcf191
Liviu <petrealiviu1996@gmail.com>
2023-11-16 12:57:27 +0000

    Change script run

--- sample/saple_build_run.sh
+++ sample/saple_build_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Удаляем существующий Docker-образ
-#docker image rm helloworld
+docker stop Hello_World
+docker rm Hello_World
 
 # Собираем новый Docker-образ
-docker build -t helloworld /var/helloworld
+docker build -t helloworld ..
 
 # Запускаем контейнер
 docker run -d --name Hello_World helloworld
